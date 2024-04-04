077c5449c3e45ede9a5ae156c1a4565278c0ebd8
sangnnbs <nguyennhubaosang@gmail.com>
2024-04-05 00:50:36 +0700

    backend: add command input for db

--- backend/server.js
+++ backend/server.js
@@ -21,15 +21,43 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to TODO application." });
 });
 
-require("./routes/todo.routes.js")(app);
-
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  try {
-    console.log(`Server is running on port ${PORT}.`);
-  } catch (error) {
-    console.log("Cannot start server");
-    console.log(error);
+
+const readline = require("node:readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Enter your DB (or type 'default): ",
+});
+
+rl.prompt();
+
+rl.on("line", (input) => {
+  if (input === "default") {
+    console.log("Correct input!");
+    rl.close();
+    // Server Starts here
+    require("./routes/todo.routes.js")(app);
+    app.listen(PORT, () => {
+      try {
+        console.log(`Server is running on port ${PORT}.`);
+      } catch (error) {
+        console.log("Cannot start server");
+        console.log(error);
+      }
+    });
+  } else {
+    console.log("Invalid input, please try again.");
+    rl.prompt();
   }
+}).on("close", () => {
+  console.log("Exiting the readline interface...");
+});
+
+rl.on("SIGINT", () => {
+  console.log("Interrupted by user, closing the readline interface...");
+
+  rl.close();
 });
