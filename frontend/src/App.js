838bbfc2d85d56f16328fc2d5cb52a14fecb442e
sangnnbs <nguyennhubaosang@gmail.com>
2024-05-15 17:33:27 +0700

    frontend: change justify at circle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,39 +2,16 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import Item from './components/Item'
 function App() {
-    const [data, setData] = useState([])
-    // useEffect(() => {
-    //     fetch('http://localhost:8080/api/todos')
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             setData(data)
-    //         })
-    // }, [])
+    const [check, setCheck] = useState(false)
 
-    // useEffect(() => {
-    //  for auto-sync: my idea
-    //  other way: useReducer
-    // }, [data])
+    const handleClick = () => {
+        setCheck(!check)
+    }
 
     return (
         <div className="App">
             <h1>TODO App asdasdasds</h1>
-            {/* {data.map((dt, index) => (
-                <div key={index}>
-                    {dt.name} + {dt.id} + {dt.description}
-                </div>
-            ))}
-            <button
-                onClick={() => {
-                    fetch('http://localhost:8080/api/todos/7', {
-                        method: 'DELETE',
-                    })
-                }}
-            >
-                Delete id 1
-            </button> */}
-            <Item />
-            <Item />
+            <Item status={check} onClick={handleClick} />
         </div>
     )
 }
--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,29 +1,42 @@
 import React, { useState } from 'react'
 import clsx from 'clsx'
 
-const Item = () => {
-    const [status, setStatus] = useState(true) // component state
-    const handleClick = () => {
-        // component handleClick
-
-        setStatus(!status)
-    }
-    const buttonClass = clsx('rounded-full w-10 h-5', {
+const Item = ({ status, onClick }) => {
+    const buttonClass = clsx(
+        'rounded-s-full rounded-e-full flex flex-row items-center',
+        {
+            // component hooked style
+            'bg-green-600': status,
+            'bg-red-600': !status,
+        }
+    )
+    const statusClass = clsx('w-2 h-2 rounded-full', {
         // component hooked style
         'bg-green-600': status,
         'bg-red-600': !status,
     })
+    const movingClass = clsx('w-10 h-5 flex flex-row items-center', {
+        // component hooked style
+        'justify-start': status,
+        'justify-end': !status,
+    })
 
     return (
-        <div className="container mx-auto w-80 h-10 flex items-center p-10 justify-center bg-neutral-800 text-white rounded-lg ">
-            <div className="container flex justify-center items-center">
-                <div className="w-20 h-5 bg-red-600"></div>
+        <div className="container mx-auto w-80 h-20 flex items-center p-10 justify-center bg-neutral-800 text-white rounded-s-full rounded-e-full ">
+            <div className="container flex justify-center items-center gap-x-1">
+                <div className="w-1 h-10 rounded-b-full rounded-t-full  bg-red-600"></div>
                 <div className="container">
                     <div className="">Title</div>
                     <div className="">Description</div>
                 </div>
             </div>
-            <button className={buttonClass} onClick={handleClick}></button>
+            <button className={buttonClass} onClick={onClick}>
+                <div className={movingClass}>
+                    <div className="flex flex-row items-center justify-center w-5 h-5 rounded-full bg-white">
+                        <div className={statusClass}></div>
+                    </div>
+                </div>
+            </button>
         </div>
     )
 }
